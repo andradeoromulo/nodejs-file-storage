cd18f391375da45522bc48354d3a5e755683afbc
Rômulo Andrade <minduim@odyssey.localdomain>
2020-10-29 16:02:35 -0300

    Add base code

---
+++ FileManager.js
@@ -0,0 +1,82 @@
+const { writeFileSync, readFileSync } = require('fs');
+
+class FileManager {
+    constructor(filePath) {
+        this.FILE_PATH = filePath;
+    }
+
+    async writeFileData(data) {
+        await writeFileSync(this.FILE_PATH, JSON.stringify(data));
+        return true;
+    }
+
+    async fetchFileData() {
+        /* 
+         * Reading the JSON file with readFile as a Promise.
+         * We could just require the JSON file, but the goal is to practice the filesystem management.
+        */
+        const file = await readFileSync(this.FILE_PATH, "utf-8");
+        return JSON.parse(file.toString());
+    }
+
+    async create(item) {
+        const existingData = await this.fetchFileData();
+
+        const updatedData = [
+            ...existingData,
+            item
+        ];
+
+        const result = await this.writeFileData(updatedData);
+
+        return result;
+
+    }
+    
+    async list(id) {
+        const data = await this.fetchFileData();
+
+        /*
+         * If an id has been informed, we'll filter the data by the id.
+         * Otherwise, we'll just send all the data.
+        */
+        const filteredData = data.filter(item => 
+            (id 
+                ? (item.id === id) 
+                : true)
+        ); 
+
+        return filteredData;
+    }
+
+    async update(id, updatedItem) {
+        const existingData = await this.fetchFileData();
+
+        const itemIndex = existingData.findIndex((item) => item.id === parseInt(id));
+
+        if(itemIndex === -1)
+            throw new Error("Not such item in the file");
+        
+        existingData[itemIndex] = updatedItem;
+        
+        return await this.writeFileData(existingData);
+    }
+
+    async delete(id) {
+        if(!id) 
+            return await this.writeFileData([]);
+
+        const existingData = await this.fetchFileData();  
+
+        const itemIndex = existingData.findIndex((item) => item.id === parseInt(id));
+        
+        if(itemIndex === -1) 
+            throw new Error("Not such item in the file");
+        
+        existingData.splice(itemIndex, 1);
+
+        return await this.writeFileData(existingData);
+    }
+}
+
+module.exports = FileManager;
---
+++ test.js
@@ -0,0 +1,52 @@
+const { deepStrictEqual, ok } = require("assert");
+const { DEFAULT_ECDH_CURVE } = require("tls");
+const FileManager = require('./FileManager');
+
+const charactherFileManager = new FileManager("./data/characters.json");
+
+const CHARACTER_SCHEMA = {
+    "id": 1,
+    "name": "Penny",
+    "birthday": "Fall 2nd",
+    "gifts": ["Diamond", "Emerald", "Melon", "Poppy"]
+};
+
+describe("Stardew Characters CRUD", () => {
+
+    it("Must be able to create a character in the file", async() => {
+        const expected = CHARACTER_SCHEMA;
+        const result = await charactherFileManager.create(CHARACTER_SCHEMA);
+        const [insertedCharacter] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
+
+        deepStrictEqual(insertedCharacter, expected);
+    });
+
+    it("Must be able to fetch an specific character in the file", async() => {
+        const expected = CHARACTER_SCHEMA;
+        const [result] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
+        
+        deepStrictEqual(result, expected);
+    });
+
+    it("Must be able to update an specific character in the file", async() => {
+        const expected = {
+            "id": 1,
+            "name": "Penny",
+            "birthday": "Fall 2nd",
+            "gifts": ["Poppyseed Muffin", "Red Plate", "Roots Platter", "Sandfish"]
+        };
+
+        const result = await charactherFileManager.update(CHARACTER_SCHEMA.id, expected);
+        const [updatedCharacter] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
+
+        ok(updatedCharacter, expected);
+    });
+
+    it("Must be able to delete an specific character in the file", async() => {
+        const expected = true;
+        const result = await charactherFileManager.delete(CHARACTER_SCHEMA.id);
+
+        ok(result, expected);
+    });
+
+});

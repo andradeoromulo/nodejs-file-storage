95cd91cc18253e7905a87197ddd68e00856bffcb
Rômulo Andrade <minduim@odyssey.localdomain>
2020-11-02 12:39:32 -0300

    Add JSON Schema Validation to the tests

--- test.js
+++ test.js
@@ -1,52 +1,78 @@
-const { deepStrictEqual, ok } = require("assert");
-const { DEFAULT_ECDH_CURVE } = require("tls");
+const chai = require('chai');
+chai.use(require('chai-json-schema-ajv'));
+const expect = chai.expect;
+const { ok } = require("assert");
+
 const FileManager = require("./FileManager");
 
 const charactherFileManager = new FileManager("./data/characters.json");
 
-const CHARACTER_SCHEMA = {
-    "id": "1",
-    "name": "Penny",
-    "birthday": "Fall 2nd",
-    "gifts": ["Diamond", "Emerald", "Melon", "Poppy"]
+const INSERTION_TEST_CHARACTER = {
+    "id": "*",
+    "name": "ConcernedApe",
+    "birthday": "December 3rd",
+    "gifts": ["An amazing community", "Sharing Experiences"]
+};
+
+const UPDATE_TEST_CHARACTER = {
+    "name": "Eric Barone a.k.a. ConcernedApe"
+};
+
+const CHARACTHER_JSON_SCHEMA = {
+    title: "Character JSON Schema",
+    type: "array",
+    minItems: 1,
+    items: {
+        type: "object",
+        required: ["id", "name", "birthday", "gifts"],
+        properties: {
+            id: {
+                type: "string"
+            },
+            name: {
+                type: "string"
+            },
+            birthday: {
+                type: "string"
+            },
+            gifts: {
+                type: "array",
+                minItems: 1,
+                uniqueItems: true,
+                items: {
+                    type: "string"
+                }
+            }
+        }
+    }
 };
 
 describe("Stardew Characters CRUD", () => {
 
     it("Must be able to create a character in the file", async() => {
-        const expected = CHARACTER_SCHEMA;
-        const result = await charactherFileManager.create(CHARACTER_SCHEMA);
-        const [insertedCharacter] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
-
-        deepStrictEqual(insertedCharacter, expected);
+        await charactherFileManager.create(INSERTION_TEST_CHARACTER);
+        const insertedCharacter = await charactherFileManager.list(INSERTION_TEST_CHARACTER.id);
+       
+        expect(insertedCharacter).to.be.jsonSchema(CHARACTHER_JSON_SCHEMA, 'Result not consistent to the JSON-Schema Validation');
     });
 
     it("Must be able to fetch an specific character in the file", async() => {
-        const expected = CHARACTER_SCHEMA;
-        const [result] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
+        const result = await charactherFileManager.list(INSERTION_TEST_CHARACTER.id);
         
-        deepStrictEqual(result, expected);
+        expect(result).to.be.jsonSchema(CHARACTHER_JSON_SCHEMA, 'Result not consistent to the JSON-Schema Validation');
     });
 
     it("Must be able to update an specific character in the file", async() => {
-        const expected = {
-            "id": "1",
-            "name": "Penny",
-            "birthday": "Fall 2nd",
-            "gifts": ["Poppyseed Muffin", "Red Plate", "Roots Platter", "Sandfish"]
-        };
+        await charactherFileManager.update(INSERTION_TEST_CHARACTER.id, UPDATE_TEST_CHARACTER);
+        const updatedCharacter = await charactherFileManager.list(INSERTION_TEST_CHARACTER.id);
 
-        const result = await charactherFileManager.update(CHARACTER_SCHEMA.id, expected);
-        const [updatedCharacter] = await charactherFileManager.list(CHARACTER_SCHEMA.id);
-
-        ok(updatedCharacter, expected);
+        expect(updatedCharacter).to.be.jsonSchema(CHARACTHER_JSON_SCHEMA, 'Result not consistent to the JSON-Schema Validation');
     });
 
     it("Must be able to delete an specific character in the file", async() => {
-        const expected = true;
-        const result = await charactherFileManager.delete(CHARACTER_SCHEMA.id);
+        const result = await charactherFileManager.delete(INSERTION_TEST_CHARACTER.id);
 
-        ok(result, expected);
+        ok(result, true);
     });
 
 });
